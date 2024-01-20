0798f4630c1578df31903e21161cbc3385a86327
Rolando HR <rolando167@hotmail.com>
2024-01-20 11:27:35 +0100

    Cambios  2024 - Docker

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-	res.render('index.html', { title:'Bienvenido SR.' });
+	res.render('index.html', { title:'Bienvenido SR. 😄' });
 	// console.log(path.join(__dirname, 'views/index.html'));// para qu no afecte \ ó / 
 });
 
